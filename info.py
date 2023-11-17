aa2b020d0e7fdaa324f5f0ba93279c6016caba51
dhanush2bot <149936819+dhanush2bot@users.noreply.github.com>
2023-11-17 22:31:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,17 +13,17 @@ def is_enabled(value, default):
         return default
 
 # Bot information
-API_ID = environ.get('API_ID', '')
+API_ID = environ.get('API_ID', '23081466')
 if len(API_ID) == 0:
     logging.error('API_ID is missing, exiting now')
     exit()
 else:
     API_ID = int(API_ID)
-API_HASH = environ.get('API_HASH', '')
+API_HASH = environ.get('API_HASH', 'dbc665db1489f9d3cfd8de4a52f1ad4b')
 if len(API_HASH) == 0:
     logging.error('API_HASH is missing, exiting now')
     exit()
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6825398442:AAFLP2CGjWrMoJWyB_pw3XKOINfnwfIFDhA')
 if len(BOT_TOKEN) == 0:
     logging.error('BOT_TOKEN is missing, exiting now')
     exit()
@@ -33,7 +33,7 @@ PORT = int(environ.get('PORT', '8080'))
 PICS = (environ.get('PICS', 'https://telegra.ph//file/be2a8f0b2ef67b6568b9a.jpg https://telegra.ph//file/e16477ed586a16dea889f.jpg https://telegra.ph//file/59abc4beca9e1709256af.jpg https://telegra.ph//file/1ab4a7534e2a3091dd5ae.jpg https://telegra.ph//file/1ab4a7534e2a3091dd5ae.jpg https://telegra.ph//file/8b5a4ce8fc357015f2630.jpg https://telegra.ph//file/9674c602043145825183b.jpg https://telegra.ph//file/c1be6af5026ca6185d21a.jpg https://telegra.ph//file/96ff7dc3a3103e73ee14e.jpg https://telegra.ph//file/408eb85b765822466bb9a.jpg https://telegra.ph//file/38c72de01271608615214.jpg https://telegra.ph//file/ade919d82373b8043ec7b.jpg')).split()
 
 # Bot Admins
-ADMINS = environ.get('ADMINS', '')
+ADMINS = environ.get('ADMINS', '1764208280')
 if len(ADMINS) == 0:
     logging.error('ADMINS is missing, exiting now')
     exit()
@@ -50,7 +50,7 @@ if len(LOG_CHANNEL) == 0:
 else:
     LOG_CHANNEL = int(LOG_CHANNEL)
     
-SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '')
+SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '-1001982864590')
 if len(SUPPORT_GROUP) == 0:
     logging.error('SUPPORT_GROUP is missing, exiting now')
     exit()
@@ -63,15 +63,15 @@ if len(OPENAI_API) == 0:
     exit()
 
 # MongoDB information
-DATABASE_URL = environ.get('DATABASE_URL', "")
+DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://INDEX:INDEX@cluster0.wlf4emj.mongodb.net/?retryWrites=true&w=majority")
 if len(DATABASE_URL) == 0:
     logging.error('DATABASE_URL is missing, exiting now')
     exit()
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "INDEX")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Links
-SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/BotGeniusProbot')
+SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/filmyspotmovie')
 UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/filmyspotupdate')
 
 # Bot settings
@@ -93,19 +93,19 @@ FILE_CAPTION = environ.get("FILE_CAPTION", script.FILE_CAPTION)
 SHORTLINK_URL = environ.get("SHORTLINK_URL", "shareus.io")
 SHORTLINK_API = environ.get("SHORTLINK_API", "0ft2s2mKCHOaLUKDZCcnDXCt8O63")
 VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
-IS_VERIFY = is_enabled(environ.get("IS_VERIFY", "False"), False)
+IS_VERIFY = is_enabled(environ.get("IS_VERIFY", "True"), True)
 WELCOME_TEXT = environ.get("WELCOME_TEXT", script.WELCOME_TEXT)
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/filmyspotupdate")
 INDEX_EXTENSIONS = [extensions.lower() for extensions in environ.get('INDEX_EXTENSIONS', 'mp4 mkv').split()]
 
 # stream features vars
-BIN_CHANNEL = environ.get("BIN_CHANNEL", "")
+BIN_CHANNEL = environ.get("BIN_CHANNEL", "-1002028796606")
 if len(BIN_CHANNEL) == 0:
     logging.error('BIN_CHANNEL is missing, exiting now')
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "")
+URL = environ.get("URL", "auto-filter-sved.onrender.com")
 if len(URL) == 0:
     logging.error('URL is missing, exiting now')
     exit()
