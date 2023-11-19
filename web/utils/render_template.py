4d813fed35446f0e40a9ead8beff7596e70a017f
dhanush2bot <149936819+dhanush2bot@users.noreply.github.com>
2023-11-19 10:35:03 +0530

    Update render_template.py

--- web/utils/render_template.py
+++ web/utils/render_template.py
@@ -1,29 +1,9 @@
-from info import BIN_CHANNEL, URL
-from utils import temp
-from web.utils.custom_dl import TGCustomYield
-from utils import get_size
-import urllib.parse
-import secrets
-import mimetypes
-import aiofiles
-import logging
-import aiohttp
-
-async def fetch_properties(message_id):
-    media_msg = await temp.BOT.get_messages(BIN_CHANNEL, message_id)
-    file_properties = await TGCustomYield().generate_file_properties(media_msg)
-    file_name = file_properties.file_name if file_properties.file_name \
-        else f"{secrets.token_hex(2)}.jpeg"
-    mime_type = file_properties.mime_type if file_properties.mime_type \
-        else f"{mimetypes.guess_type(file_name)}"
-    return file_name, mime_type
-
-
 async def render_page(message_id):
     file_name, mime_type = await fetch_properties(message_id)
     src = urllib.parse.urljoin(URL, str(message_id))
     audio_formats = ['audio/mpeg', 'audio/mp4', 'audio/x-mpegurl', 'audio/vnd.wav']
     video_formats = ['video/mp4', 'video/avi', 'video/ogg', 'video/h264', 'video/h265', 'video/x-matroska']
+    heading = ''  # Define heading here
     if mime_type.lower() in video_formats:
         async with aiofiles.open('web/template/req.html') as r:
             heading = 'Watch {}'.format(file_name)
