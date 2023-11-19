7859df49619f9b400d7c59c1e045102b960b81da
dhanush2bot <149936819+dhanush2bot@users.noreply.github.com>
2023-11-19 15:33:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,42 +35,51 @@ class Bot(Client):
         )
 
     async def start(self):
-        temp.START_TIME = time.time()
-        b_users, b_chats = await db.get_banned()
-        temp.BANNED_USERS = b_users
-        temp.BANNED_CHATS = b_chats
-        await super().start()
-        if os.path.exists('restart.txt'):
-            with open("restart.txt") as file:
-                chat_id, msg_id = map(int, file)
+        try:
+            temp.START_TIME = time.time()
+            b_users, b_chats = await db.get_banned()
+            temp.BANNED_USERS = b_users
+            temp.BANNED_CHATS = b_chats
+            await super().start()
+            if os.path.exists('restart.txt'):
+                with open("restart.txt") as file:
+                    chat_id, msg_id = map(int, file)
+                try:
+                    await self.edit_message_text(chat_id=chat_id, message_id=msg_id, text='Restarted Successfully!')
+                except:
+                    pass
+                os.remove('restart.txt')
+            temp.BOT = self
+            await Media.ensure_indexes()
+            me = await self.get_me()
+            temp.ME = me.id
+            temp.U_NAME = me.username
+            temp.B_NAME = me.first_name
+            username = '@' + me.username
+            app = web.AppRunner(await web_server())
+            await app.setup()
+            await web.TCPSite(app, "0.0.0.0", PORT).start()
+            logging.info(f"\n\nBot [{username}] Started!\n\n")
             try:
-                await self.edit_message_text(chat_id=chat_id, message_id=msg_id, text='Restarted Successfully!')
+                await self.send_message(chat_id=LOG_CHANNEL, text=f"<b>{me.mention} Restarted! 🤖</b>")
             except:
-                pass
-            os.remove('restart.txt')
-        temp.BOT = self
-        await Media.ensure_indexes()
-        me = await self.get_me()
-        temp.ME = me.id
-        temp.U_NAME = me.username
-        temp.B_NAME = me.first_name
-        username = '@' + me.username
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        await web.TCPSite(app, "0.0.0.0", PORT).start()
-        logging.info(f"\n\nBot [{username}] Started!\n\n")
-        try:
-            await self.send_message(chat_id=LOG_CHANNEL, text=f"<b>{me.mention} Restarted! 🤖</b>")
-        except:
-            logging.error("Make sure bot admin in LOG_CHANNEL, exiting now")
+                logging.error("Make sure bot admin in LOG_CHANNEL, exiting now")
+                exit()
+            try:
+                m = await self.send_message(chat_id=BIN_CHANNEL, text="Test")
+                await m.delete()
+            except:
+                logging.error("Make sure bot admin in BIN_CHANNEL, exiting now")
+                exit()
+        except AccessTokenExpired:
+            logging.error("Access token has expired. Please check your credentials.")
             exit()
-        try:
-            m = await self.send_message(chat_id=BIN_CHANNEL, text="Test")
-            await m.delete()
-        except:
-            logging.error("Make sure bot admin in BIN_CHANNEL, exiting now")
+        except AccessTokenInvalid:
+            logging.error("Access token is invalid. Please check your credentials.")
             exit()
-
+        except Exception as e:
+            logging.error(f"An error occurred during bot startup: {e}")
+            exit()
 
     async def stop(self, *args):
         await super().stop()
