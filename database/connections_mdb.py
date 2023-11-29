34385454d30afcbab70f632f205d8285373fd08f
dhanush2bot <149936819+dhanush2bot@users.noreply.github.com>
2023-11-29 23:54:48 +0530

    Update connections_mdb.py

--- database/connections_mdb.py
+++ database/connections_mdb.py
@@ -9,13 +9,13 @@ myclient = pymongo.MongoClient(DATABASE_URL)
 mydb = myclient[DATABASE_NAME]
 mycol = mydb['Connections']   
 
-async def add_connection(group_id, user_id):
+async def add_connection(group_id, user_id, premium=False):
     user = mycol.find_one({'id': user_id})
     if user:
         if group_id not in user["ids"]:
             mycol.update_one({'id': user_id}, {"$push": {"ids": group_id}})
     else:
-        mycol.insert_one({'id': user_id, 'ids': [group_id]})
+        mycol.insert_one({'id': user_id, 'ids': [group_id], 'premium': premium})
 
 async def all_connections(user_id):
     user = mycol.find_one({'id': user_id})
@@ -26,3 +26,14 @@ async def all_connections(user_id):
 
 async def delete_connections(group_id):
     mycol.update_many({"ids": group_id}, {"$pull": {"ids": group_id}})
+
+async def set_premium_status(user_id, premium=True):
+    user = mycol.find_one({'id': user_id})
+    if user:
+        mycol.update_one({'id': user_id}, {"$set": {"premium": premium}})
+    else:
+        logger.error(f"User {user_id} not found in the database.")
+
+async def get_premium_status(user_id):
+    user = mycol.find_one({'id': user_id})
+    return user.get("premium", False) if user else False
